5677be16b65507e81492c848acf68fe0e68d3389
chaosagex <hk2nasr@gmail.com>
2013-12-18 21:50:43 +0200

    new kth method

--- [TEMPLATE] ImageFilters/ImageFilters/kth element.cs
+++ [TEMPLATE] ImageFilters/ImageFilters/kth element.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication1
+namespace Sort
 {
      class kth_element
      {
@@ -25,8 +25,8 @@ namespace ConsoleApplication1
                int end=arr.Length-1;
                for (int l = 0; l < k; l++)
                {
-                   index=smallest(ref arr,begin,end);
-                   Swap(ref arr[index],ref arr[begin]);
+                    index = smallest(ref arr, begin, end);
+                    Swap(ref arr[index], ref arr[begin]);
                     begin++;
                     index = largest(ref arr, begin, end);
                     Swap(ref arr[index], ref arr[end]);
@@ -39,6 +39,13 @@ namespace ConsoleApplication1
 
           private int smallest(ref byte[] arr, int begin, int end)
           {
+               if (begin + 1 == end)
+               {
+                    if (arr[begin] > arr[end])
+                         return end;
+                    else
+                         return begin;
+               }
                if (begin == end)
                     return end;
                int i = begin+1;
@@ -47,17 +54,28 @@ namespace ConsoleApplication1
                while (j > i)
                {
                     while (arr[i] <= arr[pivot])
+                    {
+                         if (i == end)
+                              return smallest(ref arr, begin+1, j); ;
                          i++;
+                    }
                     while (arr[j] > arr[pivot])
                          j--;
                     if (i < j)
                          Swap(ref arr[i], ref arr[j]);
                }
                Swap(ref arr[j], ref arr[pivot]);
-               return smallest(ref arr, 0, j - 1); ;
+               return smallest(ref arr, begin, j);
           }
           private int largest(ref byte[] arr, int begin, int end)
           {
+               if (begin + 1 == end)
+               {
+                    if (arr[begin] < arr[end])
+                         return end;
+                    else
+                         return begin;
+               }
                if(begin==end)
                     return end;
                int i = begin+1;
