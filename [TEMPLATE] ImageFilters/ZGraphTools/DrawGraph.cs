7567391c0f76085234fa747dc40dc5f2d8b44cb2
chaosagex <hk2nasr@gmail.com>
2014-01-13 21:26:27 +0200

    preprations to draw graph
    kol wa7d sh3'al f filter ysheel el "//operation code " w y7ot mkano el
    call ele by5le el 7et bt3to tsht3'lmen 3'er ama y3rd 7aga
    
    PS:gui designer shoof tre2a ta5od beha el wMax w 3shan tst5dm el class
    bta3 drawGraph call the function "draw()" MT3MLSH INSTANCE

---
+++ [TEMPLATE] ImageFilters/ZGraphTools/DrawGraph.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZGraphTools;
+
+namespace ZGraphTools
+{
+     class DrawGraph
+     {
+
+          public void draw(int wMax, Graph g)
+          {
+           ZGraphForm z;
+           int size = 3;
+           double[] x;
+           double[] y;
+           int index = 0;
+           int before;
+           int after;
+               if (g == Graph.ADAPTIVE_MEDIAN)
+                    z = new ZGraphForm("Adaptive Median", "Window Size", "Time");
+               else
+                    z = new ZGraphForm("Alpha Trim", "Window Size", "Time");
+               x = new double[wMax];
+               y = new double[wMax];
+               while (size < wMax)
+               {
+                    
+                    if (g == Graph.ADAPTIVE_MEDIAN)
+                    {
+                         before = System.Environment.TickCount;
+                         //operation code 
+                    }
+                    else
+                    {
+                         before = System.Environment.TickCount;
+                         //operation code  
+                    }
+                    after = System.Environment.TickCount;
+                    y[index] = after - before;
+                    x[index] = size;
+                    size += 2;
+                    index++;
+               }
+               z.add_curve("Name goes here", x, y, System.Drawing.Color.Black);
+          }
+     }
+}
---
+++ [TEMPLATE] ImageFilters/ZGraphTools/Graphs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZGraphTools
+{
+     enum Graph
+     {
+          ALPHA_TRIM = 0,
+          ADAPTIVE_MEDIAN = 1
+     }
+}
