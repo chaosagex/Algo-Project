3225f36517d75c2fe33df7856f8b0fa231ef1b1b
chaosagex <hk2nasr@gmail.com>
2014-01-14 19:13:35 +0200

    some changes in counting sort
    counting sort nw takes byte arr  and returns byte array , modded code to
    avoid waste of time to use the code use .sort function DONT CREATE AN
    INSTANCE

--- [TEMPLATE] ImageFilters/ImageFilters/countingsort.cs
+++ [TEMPLATE] ImageFilters/ImageFilters/countingsort.cs
@@ -4,48 +4,55 @@ using System.Text;
 
 namespace Sort
 {
-	class countingsort
-	{
-		static public int[] CountingSort(int [] input)
-        {
-            // O(1)
-           int max = input[0];
-
-            // O(N)
-            for (int i = 0; i < input.Length; i++)
-            {
-               if (input[i] > max)
-                {
-                    max = input[i];
-                }
-            }
-
-            // Space complexity O(N+K)
-            int[] counts = new int[max + 1];
-            int[] output = new int[input.Length];
+     class CountingSort
+     {
+          static public byte[] sort(byte[] input)
+          {
+               // O(1)
+               byte max = input[0];
+               int length = input.Length;
 
 
-            // O(N)
-            for (int i = 0; i < input.Length; i++)
-            {
-                counts[input[i]]++;
-            }
+               // O(N)
+               for (byte i = 0; i < length; i++)
+               {
+                    if (input[i] > max)
+                    {
+                         max = input[i];
+                    }
+               }
 
-            int j=0;
-            // O(N+K)
-            for (int i = 0; i < counts.Length; i++)
-            {
-                while (counts[i] > 0)
-                {
-                    output[j] = i;
-                    j++;
-                    counts[i]--;
-                }
-            }
+               // Space complexity O(N+K)
+               byte[] counts = new byte[max + 1];
+               byte[] output = new byte[length];
+               int length2 = counts.Length;
 
-            return output;
-        }
-    }
+               // O(N)
+               for (byte i = 0; i < length; i++)
+               {
+                    counts[input[i]]++;
+               }
 
-	}
+               byte j = 0;
+               // O(N+K)
+               for (byte i = 0; i < length2; i++)
+               {
+                    bool flag = false;
+                    while (counts[i] > 0)
+                    {
+                         if (i == 255)
+                              flag = true;
+                         output[j] = i;
+                         j++;
+                         counts[i]--;
+                    }
+                    if (flag == true)
+                         break;
+               }
+
+               return output;
+          }
+     }
+
+}
 
