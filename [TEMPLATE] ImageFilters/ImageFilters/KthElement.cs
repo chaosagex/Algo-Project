3c153bba0bbe2376d7535962ea137431bc2d14ee
chaosagex <hk2nasr@gmail.com>
2014-01-16 17:22:09 +0200

    other standard kth method
    lw shayf tree2a asr3 eb2a 2olole :D

--- [TEMPLATE] ImageFilters/ImageFilters/KthElement.cs
+++ [TEMPLATE] ImageFilters/ImageFilters/KthElement.cs
@@ -9,17 +9,22 @@ namespace Sort
           public static byte[] sort(ref byte[] arr, int k)
           {
                int index;
-               int begin = 0;
+               byte begin = 0;
                int end = arr.Length - 1;
+               int length = arr.Length;
+               List<byte> ar = new List<byte>(arr);
                for (int l = 0; l < k; l++)
                {
-                    index = smallest(ref arr, begin, end);
+                    index = elementAt(ar, 1, ar, true, begin, end + 1);
                     Swap(ref arr[index], ref arr[begin]);
                     begin++;
-                    index = largest(ref arr, begin, end);
+                    ar = new List<byte>(arr);
+                    index = elementAt(ar, length, ar, true, begin, end + 1);
                     Swap(ref arr[index], ref arr[end]);
                     end--;
+                    ar = new List<byte>(arr);
                }
+               // Console.Out.WriteLine(elementAt(ar, 1,ar,true));
                return arr;
           }
 
@@ -91,6 +96,45 @@ namespace Sort
                temp = lhs;
                lhs = rhs;
                rhs = temp;
+          }
+          private static int elementAt(List<byte> A, int k, List<byte> m, bool first, byte pivot, int end)
+          {
+               int n;
+               List<byte> A1 = new List<byte>();
+               List<byte> A2 = new List<byte>();
+               if (first)
+                    n = end;
+               else
+                    n = A.Count;
+
+
+               for (byte i = pivot; i < n; i++)
+               {
+                    if (!first)
+                         pivot = A[0];
+                    if (first)
+                    {
+                         if (A[i] < A[pivot])
+                              A1.Add(i);
+                         else if (A[i] > A[pivot])
+                              A2.Add(i);
+                    }
+                    else
+                    {
+                         if (m[A[i]] < m[pivot])
+                              A1.Add(A[i]);
+                         else if (m[A[i]] > m[pivot])
+                              A2.Add(A[i]);
+                    }
+
+               }
+
+               if (k <= A1.Count)
+                    return elementAt(A1, k, m, false, 0, end);
+               else if (k > A.Count - A2.Count)
+                    return elementAt(A2, k - (A.Count - A2.Count), m, false, 0, end);
+               else
+                    return pivot;
           }
      }
 }
